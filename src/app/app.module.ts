194e52f6fbce0e2215af2034dcce9f6f68463ced
sws <sws@paywith.co.kr>
2021-02-12 22:48:34 +0900

    in-memory-data 등록 및 초기값 설정

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientModule,
+    //데이터 서버와 주고받음을 흉내내기 위해 해당 모듈 설치
+    //InMemoryDataService 등록하기 위해 InMemoryDbService를 구현해야한다.
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+      dataEncapsulation: false,
+    }),
+  ],
+
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InMemoryDataService implements InMemoryDbService {
+  constructor() {}
+  /*   createDb(reqInfo?: RequestInfo): {} | Observable<{}> | Promise<{}> {
+    throw new Error('Method not implemented.');
+  } */
+
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Dr Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' },
+    ];
+    return { heroes };
+  }
+
+  /**
+   *
+   * hearo가 비어있으면 초기값 11
+   * 비어있지 않으면 가장 높은 번호를 가진 id에 +1
+   * @param {Hero[]} heroes
+   * @return {*}  {number}
+   * @memberof InMemoryDataService
+   */
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0
+      ? Math.max(...heroes.map((hero) => hero.id)) + 1
+      : 11;
+  }
+}
